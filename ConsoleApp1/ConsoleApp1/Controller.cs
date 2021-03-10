ca10119814a45633706da00472daf360e2816e6d
Dgouss <50356290+Dgouss@users.noreply.github.com>
2021-03-10 12:23:39 +0100

    Début De fonctionnalité de comptage de caractère

---
+++ ConsoleApp1/ConsoleApp1/Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    class Controller
+    {
+        public string source;
+        public void CountChar()
+        {
+            // 1.
+            // Array to store frequencies.
+            int[] c = new int[(int)char.MaxValue];
+
+            // 2.
+            // Read entire text file.
+            string s = File.ReadAllText(source);
+
+            // 3.
+            // Iterate over each character.
+            foreach (char t in s)
+            {
+                // Increment table.
+                c[(int)t]++;
+            }
+
+            // 4.
+            // Write all letters found.
+            for (int i = 0; i < (int)char.MaxValue; i++)
+            {
+                if (c[i] > 0 &&
+                    char.IsLetterOrDigit((char)i))
+                {
+                    Console.WriteLine("Letter: {0}  Frequency: {1}",
+                        (char)i,
+                        c[i]);
+                }
+            }
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        Controller c = new Controller();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program();
+        }
+
+        public Program()
+        {
+            MainMenu();
+        }
+        public void MainMenu()
+        {
+            Console.WriteLine("Welcome to our program, please choose your file Source \n");
+            Console.WriteLine("Enter source :");
+            string source = "";
+            while (!Directory.Exists(source) && !File.Exists(source))
+            {
+                source = Console.ReadLine();
+                if (!Directory.Exists(source) && !File.Exists(source))
+                {
+                    Console.WriteLine("Your source do not exist! Please try again:");
+                }
+            }
+            c.source = source;
+            c.CountChar();
+/*            Console.WriteLine("Do you want to save your search ? (y,n) :");
+            string validate = "";
+            do
+            {
+                validate = Console.ReadLine();
+
+                if (validate == "y")
+                {
+                    
+                }
+                else if (validate == "n")
+                {
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Please try again! Enter \"y\" to save and \"n\" to cancel your work:");
+                }
+            } while (validate != "y" && validate != "n");
+*/
         }
     }
+
 }
